b2bc673c848c67647039cfd22964bef3e30c8de8
Jared Stander <jaredstander@gmail.com>
2016-07-19 18:01:17 -0400

    Beginning event section for React, using onClick and bootstrap dropdown.

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,19 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
+var Dropdown = require('./dropdown');
 
 var options = {
-  thumbnailData: [
-    {
-      title: "See Tutorials",
-      number: 32,
-      header: "Learn React",
-      description: "React!",
-      imageUrl: "https://cdn.auth0.com/blog/react-js/react.png"
-    },{
-      title: "See Tutorials",
-      number: 64,
-      header: "Some other thing!",
-      description: "Rocketship!",
-      imageUrl: "https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png"
-    }
+  title: 'Choose a Dessert', // what should show up on the button to open/close the dropdown
+  items: [
+    'Chocolate Cake',
+    'Cheesecake',
+    'Vanille Cake',
+    'Straweburry Short',
+    'Fruitcake'
   ]
-};
+}
 
 // ask ract to render this class
-var element = React.createElement(ThumbnailList, options);
+var element = React.createElement(Dropdown, options);
 
 // when ask react to render this class,
 // we will tell it where to place the rendered element in the DOM
---
+++ src/dropdown.jsx
@@ -0,0 +1,25 @@
+// notes
+// show a button and a list
+// this component should know when to show the list based on when the user clicks on a button.
+
+var React = require('react');
+var Button = require('./button');
+var ListItem = require('./list-item');
+
+module.exports = React.createClass({
+  handleClick: function(){
+    alert('Button clocked dropdown.');
+  },
+  render: function(){
+    var list = this.props.items.map(function(item){
+      return <ListItem item={item} />
+    });
+
+    return <div className="dropdown">
+            <Button whenClicked={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret" />
+            <ul>
+              {list}
+            </ul>
+          </div>
+  }
+})
